a0eba362eabdf06d7ba27994cc93e08de6c8f674
Fabien Abbenbroek <fabbenbr@w-r7-p2.s19.be>
2018-10-11 16:53:17 +0200

    push

---
+++ Libft/finished tests/ft_itoa.c
@@ -0,0 +1,53 @@
+#include "libft.h"
+#include <stdio.h>
+
+void isneg(int *n, int *negative, int *length)
+{
+  if (*n < 0)
+  {
+    *n *= -1;
+    *negative = 1;
+    *length = 2;
+  }
+  else
+  {
+    *negative = 0;
+    *length = 1;
+  }
+}
+
+
+char  *ft_itoa(int n)
+{
+  int nb;
+  int length;
+  int negative;
+  char *temp;
+
+  if (n == -2147483648)
+    return ("-2147483648");
+  isneg(&n, &negative, &length);
+  nb = n;
+  while (nb /= 10)
+    length++;
+  if ((temp = ft_strnew(length)) == NULL)
+    return (NULL);
+    temp[length] = '\0';
+  while (length--)
+  {
+    temp[length] = n % 10 + '0';
+    n = n / 10;
+  }
+  if (negative == 1)
+    temp[0] = '-';
+  return (temp);
+}
+
+int main(void)
+{
+  int i;
+
+  i = -564641;
+  printf("%s", ft_itoa(i));
+  return (0);
+}
--- Libft/testing WIP/ft_strsplit.c
+++ Libft/testing WIP/ft_strsplit.c
@@ -12,10 +12,36 @@
 
 #include "libft.h"
 #include <stdio.h>
+
+int	wordcount(char const *s, char c)
+{
+	int i;
+	int words;
+	int isword;
+
+	i = 0;
+	words = 0;
+	if (s[0] != c)
+		isword = 1;
+	else
+		isword = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && isword == 1)
+			words += 1;
+
+
+		i++;
+	}
+}
 char	**ft_strsplit(char const *s, char c)
 {
-	char *temp;
-	while (s[i]
+	char **temp;
+	int i;
+	int words;
+
+	i = 0;
+	words = wordcount(s, c);
 	temp = ft_strnew(s);
 	return(0);
 }
@@ -23,7 +49,7 @@ char	**ft_strsplit(char const *s, char c)
 int main(void)
 {
 	char **str;
-	
+
 	str = ft_strsplit("*hello*fellow***students*", '*');
 	//printf("%s", ft_strsplit("*hello*fellow***students*", '*'));
 	printf("%s", *str);
