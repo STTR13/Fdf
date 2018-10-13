cf1f0254ce4f9071ffb43ebe41596995b30e2a0a
Fabien Abbenbroek <fabbenbr@w-r7-p2.s19.be>
2018-10-13 16:17:33 +0200

    push

--- Libft/finished tests/ft_strnstr.c
+++ Libft/finished tests/ft_strnstr.c
@@ -12,20 +12,22 @@
 
 #include <stdio.h>
 #include <string.h>
-char	*ft_strnstr(char *hay, const char *needle, size_t n)
+char	*ft_strnstr(const char *hay, const char *needle, size_t n)
 {
 	int x;
 	int c;
-
+    char *temp;
+    
+    temp = (char *)hay;
 	x = 0;
-	while (haystack[x] && x < n)
+	while (temp[x] && x < n)
 	{
 		c = 0;
-		while (needle[c] == haystack[c + x])
+		while (needle[c] == temp[c + x])
 		{
 			if (needle[c + 1] == '\0')
 			{
-				return (haystack + x);
+				return (temp + x);
 			}
 			c++;
 		}
@@ -36,10 +38,11 @@ char	*ft_strnstr(char *hay, const char *needle, size_t n)
 
 int main(void)
 {
-	char *str1, *str2;
+	char *str1, *str2, *str3;
 
 	str1 = "dit is een test";
 	str2 = "is";
-	printf("%s", ft_strstr(str1, str2, 7));
+    str3 = ft_strnstr(str1, str2, 7);
+	printf("%s", str3);
 	return (0);
 }
