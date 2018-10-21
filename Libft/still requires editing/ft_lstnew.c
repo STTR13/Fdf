d8d2f799adf1c0fd1eeb03a47c95b4ddf0763390
Reantsiko <42799992+Reantsiko@users.noreply.github.com>
2018-10-21 22:52:41 +0200

    push

--- Libft/still requires editing/ft_lstnew.c
+++ Libft/still requires editing/ft_lstnew.c
@@ -14,10 +14,22 @@
 
 t_list *ft_lstnew(void const *content, size_t content_size)
 {
+  t_list *nl;
 
-}
-
-int main(void)
-{
-  
+  if ((nl = (t_list *)malloc(sizeof(t_list))) == NULL)
+    return (NULL);
+  if (content == NULL)
+  {
+    nl->content = NULL;
+    nl->content_size = 0;
+  }
+  else
+  {
+    if((nl->content = malloc(sizeof(content))) == NULL)
+      return (NULL);
+    ft_memcpy((nl->content), content, sizeof(content));
+    nl->content_size = content_size;
+  }
+  nl->next = NULL;
+  return (nl);
 }
