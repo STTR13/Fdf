4c6994b92135e6fef297bb2fbed86f63adf885ee
Reantsiko <42799992+Reantsiko@users.noreply.github.com>
2018-11-04 14:47:31 +0100

    push

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,22 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fabbenbr <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/23 10:58:26 by fabbenbr          #+#    #+#             */
-/*   Updated: 2018/10/23 11:10:36 by fabbenbr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# include <stdlib.h>
-# include <fcntl.h>
-# include <unistd.h>
+# define BUFF_SIZE 1
 
-int	get_next_line(const int fd, char **line);
+# include "fcntl.h"
+# include "sys/types.h"
+# include "sys/uio.h"
+# include "unistd.h"
+# include "stdlib.h"
+# include "libft.h"
+
+int		ft_new_line(char **s, char **line, int fd, int ret);
+int		get_next_line(const int fd, char **line);
 
 #endif
