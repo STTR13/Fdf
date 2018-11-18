e18daf0ce97d3852c6f3234e25310efcb5a3b162
Reantsiko <42799992+Reantsiko@users.noreply.github.com>
2018-11-18 20:51:33 +0100

    push

---
+++ fillit/includes/fillit.h
@@ -0,0 +1,10 @@
+#ifndef FILLIT_H
+# define FILLIT_H
+# include "../Libft/libft.h"
+
+typedef struct        s_map
+{
+  char  **map
+}                     t_map;
+
+#endif
---
+++ fillit/srcs/main.c
@@ -0,0 +1,14 @@
+#include "../includes/fillit.h"
+
+int main(int argc, char **argv)
+{
+  t_map *input;
+
+
+  if (argc != 2)
+  {
+    ft_putstr("usage: ./fillit input_file\n");
+    return (1);
+  }
+  return (0);
+}
