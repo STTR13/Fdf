ab91dcd66e3f59dc47f9f52956f1cb06249d3d1f
Fabien Abbenbroek <fabbenbr@w-r9-p7.s19.be>
2019-03-14 15:26:53 +0100

    push

--- lib/libft/srcs/ft_intarrprint.c
+++ lib/libft/srcs/ft_intarrprint.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../includes/libft.h"
 
 void	ft_intarrprint(int **arr, int max_x, int max_y)
 {
--- lib/libft/srcs/get_next_line.c
+++ lib/libft/srcs/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../includes/get_next_line.h"
 
 static int		new_line(char **str, char **line, int fd, int ret)
 {
