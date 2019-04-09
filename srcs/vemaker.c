b1afa7aadb33aac34ac2f7d699ccbaa84110d89b
Fabien Abbenbroek <fabbenbr@w-r9-p7.s19.be>
2019-04-09 09:20:00 +0200

    small tweaks in vemaker.c

--- srcs/vemaker.c
+++ srcs/vemaker.c
@@ -12,17 +12,6 @@
 
 #include "../includes/fdf.h"
 
-/*static vertex		*new_grid(t_input *file)
-{
-	vertex *rvert;
-
-	rvert = NULL;
-	if (!(rvert = (vertex*)malloc(sizeof(vertex) \
-		* (file->lines * file->linelen + 1))))
-		return (NULL);
-	return (rvert);
-}*/
-
 ve					createv(int z, int x, int y)
 {
 	ve v;
@@ -33,32 +22,15 @@ ve					createv(int z, int x, int y)
 	return (v);
 }
 
-static vertex		*vertex_link(t_input *file, vertex *vert)
-{
-	int i;
-
-	i = file->lines * file->linelen;
-	while (--i >= 0)
-		vert--;
-	i = file->lines * file->linelen;
-	while (--i >= 0)
-	{
-		xlink(vert, file);
-		ylink(vert, file);
-		vert++;
-	}
-	i = file->lines * file->linelen;
-	while (--i >= 0)
-		vert--;
-	return (vert);
-}
-
-static vertex		*gridfiller(t_input *file, int x, int y, vertex *vert)
+static vertex		*gridfiller(t_input *file, vertex *vert)
 {
 	int pos;
 	int temp;
+	int x;
+	int y;
 
 	pos = 0;
+	y = 0;
 	while (y < file->lines)
 	{
 		x = 0;
@@ -76,12 +48,11 @@ static vertex		*gridfiller(t_input *file, int x, int y, vertex *vert)
 	return (vert);
 }
 
-vertex				*veconvertstart(t_input *file, int x, int y)
+vertex				*veconvertstart(t_input *file)
 {
 	vertex	*vert;
 
 	vert = NULL;
-	vert = gridfiller(file, x, y, vert);
-	//vert = vertex_link(file, vert);
+	vert = gridfiller(file, vert);
 	return (vert);
 }
--- srcs/vemaker2.c
+++ srcs/vemaker2.c
@@ -12,68 +12,50 @@
 
 #include "../includes/fdf.h"
 
-
-
-/*void	xlink(vertex *grid, t_input *file)
+static void	edgefiller_hor(t_input *f, vertex *v, edge **e)
 {
-	vertex	*temp;
+	int x;
+	int y;
 
-	if (0 < grid->v.x)
-	{
-		temp = grid - 1;
-		grid->next[2] = temp;
-	}
-	if (grid->v.x == 0 || grid->v.x < file->linelen - 1)
-	{
-		temp = grid + 1;
-		grid->next[0] = temp;
-	}
-}
-
-void	ylink(vertex *grid, t_input *file)
-{
-	vertex	*temp;
-
-	if (grid->v.y > 0)
-	{
-		temp = grid - file->linelen;
-		grid->next[3] = temp;
-	}
-	if (grid->v.y == 0 || grid->v.y < file->lines - 1)
-	{
-		temp = grid + file->linelen;
-		grid->next[1] = temp;
-	}
-}*/
-
-
-edge	*edgefiller(t_input *f, vertex *v, int x, int y)
-{
-	edge *e;
-
-	e = NULL;
+	y = 0;
 	while (y < f->lines)
 	{
 		x = 0;
 		while (x < f->linelen - 1)
 		{
-			e = add_edge(e, find_vertex(v, createv(0, x, y)), \
+			*e = add_edge(*e, find_vertex(v, createv(0, x, y)), \
 			find_vertex(v, createv(0, x + 1, y)));
 			x++;
 		}
 		y++;
 	}
+}
+
+static void	edgefiller_ver(t_input *f, vertex *v, edge **e)
+{
+	int x;
+	int y;
+
 	x = 0;
 	while (x < f->linelen)
 	{
 		y = 0;
 		while (y < f->lines - 1)
 		{
-			e = add_edge(e, find_vertex(v, createv(0, x, y)), \
+			*e = add_edge(*e, find_vertex(v, createv(0, x, y)), \
 			find_vertex(v, createv(0, x , y + 1)));
 			y++;
 		}
 		x++;
 	}
+}
+
+edge	*edgefiller(t_input *f, vertex *v)
+{
+	edge *e;
+
+	e = NULL;
+	edgefiller_hor(f, v, &e);
+	edgefiller_ver(f, v, &e);
 	return (e);
 }
