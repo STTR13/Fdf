231617fc128a216d138557ad666a8878877cfb76
Simon Taeter <staeter@w-r10-p5.s19.be>
2019-04-18 15:23:33 +0200

    fix color and work on conic projection

--- srcs/refresh.c
+++ srcs/refresh.c
@@ -29,6 +29,21 @@ static void		f1(vertex *vert, void *param)
 	);
 }
 
+/*static ve		*conic_projection_s(ve *point, warehouse *wh, ve *ret)
+{
+	double	a, b;
+	ve		tv;
+
+	a = dot_vv(minus(point, &wh->p->p, &tv), &wh->p->n);
+	b = dot_vv(minus(wh->eye, &wh->p->p, &tv), &wh->p->n);
+	if ((a > 0 && b > 0) || (a < 0 && b < 0))
+		return (nullvector(ret));
+	a = abs_s(a);
+	b = abs_s(b);
+	plus(point, scal_v(minus(point, wh->eye, &tv), a / (a + b), &tv), ret);
+	return (ret);
+}*/
+
 static void		f2(vertex *vert, void *param)
 {
 	pl tp;
