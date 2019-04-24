5420ebada9fb7e984d91206c06e511a941f7aa42
Fabien Abbenbroek <fabbenbr@w-r9-p7.s19.be>
2019-04-24 12:31:15 +0200

    gnl opti

--- srcs/file_reader_fdf.c
+++ srcs/file_reader_fdf.c
@@ -18,28 +18,35 @@
 ** t_input, which mallocs a char**.
 */
 
-static int		linelen(char *str)
+static t_bool	is_valid_linelen(char *str, int *line_len, int *line_count)
 {
 	int i;
 	int counter;
 	int j;
 
+	*line_count += 1;
 	i = 0;
 	j = 0;
 	counter = 0;
-	while (str[j] != '\0')
+	while (str[j] != '\n' && str[j] != '\0')
 		j++;
-	while ((str[i] != '\0') && i < j)
+	while ((str[i] != '\n' && str[i] != '\0') && i < j)
 	{
 		if (ft_isdigit(str[i]) == 1)
 		{
-			while ((ft_isalnum(str[i]) == 1 || str[i] == ',') && str[i] != ' ')
+			while ((ft_isalnum(str[i]) == 1 || str[i] == ',') && str[i] != ' ' && str[i] != '\n' && str[i] != '\0')
 				i++;
 			counter++;
 		}
 		i++;
 	}
-	return (counter);
+	if (*line_len == -1)
+		*line_len = counter;
+	if (str[i] == '\0')
+		return (true);
+	if (counter != *line_len)
+		return (false);
+	return (is_valid_linelen(&str[i], line_len, line_count));
 }
 
 static char		*strfiller(char *str, int i)
@@ -98,11 +105,20 @@ t_input			*file_reader_fdf(int fd)
 	char	*line;
 	t_input	*lst;
 
-	if (fd == -1 || (!(lst = ft_memalloc(sizeof(t_input)))) ||\
-	(!(lst->str = ft_strnew(OPEN_MAX))))
+	if (fd == -1 || (!(lst = ft_memalloc(sizeof(t_input))))) /* ||\
+	(!(lst->str = ft_strnew(OPEN_MAX))))*/
 		return (NULL);
+	lst->linelen = -1;
 	lst->lines = 0;
-	while (get_next_line(fd, &line) == 1)
+	if (!(lst->str = reader(fd)))
+		return (NULL);
+	close(fd);
+	if (!is_valid_linelen(lst->str, &lst->linelen, &lst->lines))
+	{
+		ft_strdel(&lst->str);
+		return (NULL);
+	}
+	/*while (get_next_line(fd, &line) == 1)
 	{
 		lst->str = ft_strjoinn(lst->str, line);
 		if (lst->lines == 0)
@@ -114,8 +130,7 @@ t_input			*file_reader_fdf(int fd)
 		}
 		lst->lines += 1;
 		free(line);
-	}
-	close(fd);
+	}*/
 	if (!(lst->input = filecreator(lst->str, lst)))
 		return (NULL);
 	ft_strdel(&lst->str);
